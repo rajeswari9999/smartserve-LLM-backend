09f7f2f5d65e10d9f9791c86c9f249d8dfbf8de7
Rajeswari <128630607+rajeswari9999@users.noreply.github.com>
2026-03-19 12:50:45 +0530

    Add files via upload

---
+++ config/smartserve.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2026 at 10:25 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `smartserve`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ai_logs`
+--
+-- Error reading structure for table smartserve.ai_logs: #1932 - Table 'smartserve.ai_logs' doesn't exist in engine
+-- Error reading data for table smartserve.ai_logs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`ai_logs`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ingredients`
+--
+-- Error reading structure for table smartserve.ingredients: #1932 - Table 'smartserve.ingredients' doesn't exist in engine
+-- Error reading data for table smartserve.ingredients: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`ingredients`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marketing_captions`
+--
+-- Error reading structure for table smartserve.marketing_captions: #1932 - Table 'smartserve.marketing_captions' doesn't exist in engine
+-- Error reading data for table smartserve.marketing_captions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`marketing_captions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_items`
+--
+-- Error reading structure for table smartserve.menu_items: #1932 - Table 'smartserve.menu_items' doesn't exist in engine
+-- Error reading data for table smartserve.menu_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`menu_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_list`
+--
+-- Error reading structure for table smartserve.purchase_list: #1932 - Table 'smartserve.purchase_list' doesn't exist in engine
+-- Error reading data for table smartserve.purchase_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`purchase_list`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recipe_details`
+--
+-- Error reading structure for table smartserve.recipe_details: #1932 - Table 'smartserve.recipe_details' doesn't exist in engine
+-- Error reading data for table smartserve.recipe_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`recipe_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table smartserve.users: #1932 - Table 'smartserve.users' doesn't exist in engine
+-- Error reading data for table smartserve.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `smartserve`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
